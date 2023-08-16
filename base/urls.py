f3c5d2a570fee795341beb8cc4acbd26ac88df7f
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-16 16:36:05 +0200

    home view & basic templates

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('mitter.urls', name='mitter')),
+    path('', include('mitter.urls')),
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
