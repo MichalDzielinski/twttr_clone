2cc0fec317428929653e7039fc448f340597529e
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-17 11:54:44 +0200

    Meeps model

--- mitter/models.py
+++ mitter/models.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+class Meep(models.Model):
+    user = models.ForeignKey(User, related_name='meeps', on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=200)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return(
+            f"{self.user} "
+            f"({self.creaed_at:%Y-%m-%d %H:%M}): "
+            f"{self.body}..."
+        )
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
