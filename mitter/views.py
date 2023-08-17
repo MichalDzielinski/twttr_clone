4f0f05f63069c17bc556e29487ecf29e4283b11b
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-17 11:51:31 +0200

    Functionality and buttons to follow & unfollow users

--- mitter/views.py
+++ mitter/views.py
@@ -18,10 +18,22 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+
+
+        if request.method == 'POST':
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()
         context = {'profile': profile}
         return render(request, 'mitter/profile.html', context )
 
-
+    else:
+        messages.success(request, ('You have to be logged in to see this page'))
+        return redirect('home')
 
 
 
